6846f410838809320e9748fef4b7fa1b0f914e56
tirostiros <tiros1@NavidogdeMacBook-Pro.local>
2014-12-22 15:14:25 +0800

    建立工程

---
+++ 2048/2048/_048AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  _048AppDelegate.h
+//  2048
+//
+//  Created by tirostiros on 14-12-22.
+//  Copyright (c) 2014年 cn.com.tiros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface _048AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 2048/2048/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  2048
+//
+//  Created by tirostiros on 14-12-22.
+//  Copyright (c) 2014年 cn.com.tiros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "_048AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([_048AppDelegate class]));
+    }
+}
